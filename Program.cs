b39a9e1bbb7ec1ed1a8e367e4175e2190210d2ed
91837 <91837@SAHIL>
2023-07-11 17:32:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿    Console.WriteLine(" enter 1 to 12 number ");
+int month = Convert.ToInt32(Console.ReadLine());    
+
+switch ( month)
+{
+    case 1:
+        Console.WriteLine("jan");
+        break;
+
+        case 2:
+        Console.WriteLine("feb");
+        break;
+
+        case 3:
+        Console.WriteLine(" mar");
+        break;
+
+        case 4:
+        Console.WriteLine(" apr");
+        break;
+
+        case 5:
+        Console.WriteLine(" may");
+        break;
+
+        case 6:
+        Console.WriteLine("jun");
+
+        break;
+
+        case 7:
+
+        Console.WriteLine("jully");
+        break;
+
+        case 8:
+        Console.WriteLine("aug");
+        break;
+
+        case 9:
+        Console.WriteLine("sep");
+        break;
+        case 10:
+        Console.WriteLine("0ct");
+
+        break;
+
+        case 11:
+        Console.WriteLine("nov");
+        break;
+
+        case 12:
+        Console.WriteLine("dec");
+
+        break;
+
+        default: Console.WriteLine("no month is there ");
+        break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
